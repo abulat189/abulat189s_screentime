20d94ed771a8977d67bfb2f722826271cd856733
abulat189 <44518454+Abulat189@users.noreply.github.com>
2023-07-29 22:16:15 +0300

    list every process

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import psutil
-x = list(psutil.process_iter())
+import time
 
-for i in range(len(x)):
-     s = x[i].name()
-     print(s)
+while 1:
+    x = list(psutil.process_iter())
+    for i in range(len(x)):
+        process = x[i]
+        running = time.time() - x[i].create_time()
+        print(process.name(), running)
+    time.sleep(10)
+
+
+
 
